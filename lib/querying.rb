ea4c2d96d4941b751598da633d22666059c97035
Barbara Washington <barbara.m.washington@gmail.com>
2018-06-22 23:09:25 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,7 +28,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title,
+  "SELECT series.title
   FROM series
   LEFT OUTER JOIN characters ON characters.series_id = series.id
   WHERE character.species = 'human'
