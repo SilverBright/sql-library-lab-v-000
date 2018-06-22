ce2701152eb3f2c52b562ab7d405ac30b4112cf7
Barbara Washington <barbara.m.washington@gmail.com>
2018-06-22 21:41:03 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,10 +1,10 @@
 INSERT INTO series
 (title, author_id, subgenre_id)
-VALUES ("Book 1"), (1), (1);
+VALUES ("Book 1", 1, 1);
 
 INSERT INTO series
 (title, author_id, subgenre_id)
-VALUES ("Book 2"), (1), (1);
+VALUES ("Book 2", 1, 1);
 
 
 INSERT INTO authors
