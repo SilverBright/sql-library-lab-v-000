526a221e4b5d8d39d0ccda72db90b640167980d4
Barbara Washington <barbara.m.washington@gmail.com>
2018-06-22 21:54:26 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,11 +3,24 @@ VALUES
   (1, 'Book 1', 1, 1),
   (2, 'Book 2', 1, 1);
 
-  INSERT INTO books
-  VALUES
-    (1, 'Book 1', 2001, 2),
-    (2, 'Book 2', 2002, 2),
-    (3, 'Book 3', 2003, 2),
-    (4, 'Book 4', 2004, 2),
-    (5, 'Book 5', 2005, 2),
-    (6, 'Book 6', 2006, 2);
+INSERT INTO books
+VALUES
+  (1, 'Book 1', 2001, 2),
+  (2, 'Book 2', 2002, 2),
+  (3, 'Book 3', 2003, 2),
+  (4, 'Book 4', 2004, 2),
+  (5, 'Book 5', 2005, 2),
+  (6, 'Book 6', 2006, 2);
+
+INSERT INTO characters
+VALUES
+  (1, "Bob", "Human", "To arms", 1, 1),
+  (1, "Joe", "Human", "Hello", 1, 1),  
+  (1, "Sally", "Orc", "Blood and Thunder", 1, 1),
+  (1, "Herman", "Elf", "Blessings be upon you", 1, 1),
+  (1, "Gary", "Cabal", "Death to the Traveler", 1, 1),
+  (1, "Cayde-6", "Exo", "My beautiful horn", 1, 1),
+  (1, "Harry Potter", "Human", "Expecto Patronus", 1, 1),
+  (1, "Tinkerbell", "Fairy", "-rings a bell-", 1, 1);
+
+  
