9a3ecd61c0ddfdc25b63fa94ec70c713620e9c7b
Barbara Washington <barbara.m.washington@gmail.com>
2018-06-22 23:09:04 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,9 @@ def select_series_title_with_most_human_characters
   "SELECT series.title,
   FROM series
   LEFT OUTER JOIN characters ON characters.series_id = series.id
-  WHERE character.species = 'human';"
+  WHERE character.species = 'human'
+  GROUP BY series.title
+  ORDER BY COUNT(character.species = 'human') DESC;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
