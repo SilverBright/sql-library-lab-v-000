ef09b8a3bd02b5f5dd42434f7fd9855f312ac0e5
Barbara Washington <barbara.m.washington@gmail.com>
2018-06-22 21:49:16 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,5 +3,11 @@ VALUES
   (1, 'Book 1', 1, 1),
   (2, 'Book 2', 1, 1);
 
--- INSERT INTO series
--- VALUES (2, 'Book 2', 1, 1);
+  INSERT INTO books
+  VALUES
+    (1, 'Book 1', 2001, 2),
+    (2, 'Book 2', 2002, 2),
+    (3, 'Book 3', 2003, 2),
+    (4, 'Book 4', 2004, 2),
+    (5, 'Book 5', 2005, 2),
+    (6, 'Book 6', 2006, 2);
